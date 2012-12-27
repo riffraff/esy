0fe33bc8a68231628cc2e24223ddf0ab0f253e87
Gabriele Renzi <rff.rff@gmail.com>
2012-12-27 21:38:33 +0100

    bit more compact

--- esy.rb
+++ esy.rb
@@ -1,74 +1,29 @@
 # coding: utf-8
-Stack = []
+Stack = [0]
 Dict = {}
 BINOPS =  %w[+ - * / & | ^]
-
-if ENV['MODE'] == 'readable'
-  START_DEF, END_DEF = 'def', 'fed'
-  START_IF, END_IF   = 'ifnot0', '0tonfi'
-  OUTC = 'outc'
-  POP  = 'pop'
-  SWAP = 'swap'
-  DUP  = 'dup'
-else
-  START_DEF, END_DEF = '☊', '☋'
-  START_IF, END_IF   = '☾', '☽'
-  OUTC = '☿'
-  POP  = '♁'
-  SWAP = '☍'
-  DUP  = '♊'
-end
-
+START_DEF, END_DEF, START_IF, END_IF, OUTC, POP, SWAP, DUP = '☊', '☋', '☾', '☽', '☿', '♁', '☍', '♊'
 
 $def = $skip = nil
 
-$stacklevel = 0
-def d *args
-  return unless $DEBUG
-  print " " * $stacklevel
-  p args 
-end
-
-def pop! 
-  Stack.pop || 0
-end
-def push! a
-  Stack.push a
-end
-def top
-  Stack.last
-end
-
 def meval l
-  d l
-  if $skip && l != END_IF
-    return :skip
-  end
-  if $def &&  l != END_DEF
-    $def << l
-    return $def
-  end
+  return :skip if $skip && l != END_IF
+  return $def << l if $def &&  l != END_DEF
   case l
   when /\A\d+\Z/
-    push! l.to_i
+    Stack.push l.to_i
   when *BINOPS
-    str = "#{pop!} #{l} #{pop!}"
-    push! eval(str)
+    Stack.push eval("#{Stack.pop} #{l} #{Stack.pop}")
   when OUTC
-    print pop!.chr
+    print Stack.pop.chr
   when POP
-    pop!
+    Stack.pop || 0
   when SWAP
-    a = pop!
-    b = pop!
-    push! a
-    push! b
+    Stack[-2], Stack[-1] = Stack[-1], Stack[-2]
   when DUP
-    push! top
+    Stack.push Stack.last
   when START_IF
-    if pop! == 0
-      $skip = true
-    end
+    $skip = Stack.pop == 0
   when END_IF
     $skip = false
   when START_DEF
@@ -77,36 +32,9 @@ def meval l
     name, *code = $def
     Dict[name] = lambda { code.each {|op| meval op } }
     $def = nil
-  when /ruby (.*)/
-    eval $1
-  when "exit"
-    exit
   else 
-    if Dict[l]
-      $stacklevel +=1
-      Dict[l].call
-      $stacklevel -=1
-    else
-      raise "unknown op #{l.inspect}"
-    end
+    Dict[l].call
   end
 end
 
-def interp! 
-  while true
-    print "D:#{Dict.keys} :: S:#{Stack.inspect} #{$def ? "!" : ">"} " 
-    res = meval gets.chomp
-    puts "=> #{res}" 
-  end
-end
-
-if __FILE__ == $0 and ARGV.empty?
-  interp!
-end
-
-
-ARGF.read.scan(/\S+/) do |word|
-  d(:word, word, $skip, $def)
-  d({dict: Dict, stack: Stack})
-  meval word
-end
+ARGF.read.scan(/\S+/) { |word|  meval word }

9566f6118d52a806f9a1dc3f5284468d9805bbcf
Gabriele Renzi <rff.rff@gmail.com>
2012-12-28 14:11:11 +0100

    move samples to their own dir

--- test.rb
+++ test.rb
@@ -1,6 +1,6 @@
 require 'minitest/autorun'
 class TestEsy < MiniTest::Unit::TestCase
-  Dir["*.esy"].each do |fn|
+  Dir["samples/*.esy"].each do |fn|
     define_method "test_#{fn}" do
      res = %x{ ruby -w esy.rb #{fn} }
      assert_equal File.read(fn.sub('esy', 'out')), res
